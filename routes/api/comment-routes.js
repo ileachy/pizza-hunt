f9816848f34a4e93ae09383b70068e68bd468216
Ian L <ileach81@gmail.com>
2022-03-02 08:15:00 -0500

    comment routes and index

--- routes/api/comment-routes.js
+++ routes/api/comment-routes.js
@@ -4,4 +4,10 @@ const {
   removeComment,
 } = require("../../controllers/comment-controller");
 
+// /api/comments/<pizzaId>
+router.route("/:pizzaId").post(addComment);
+
+// /api/comments/<pizzaId>/<commentId>
+router.route("/:pizzaId/:commentId").delete(removeComment);
+
 module.exports = router;
